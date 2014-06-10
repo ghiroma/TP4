7245db145328a985b2645d4af3c7403809540b1b
guillermo.hiroma <guillermo.hiroma@gmail.com>
2014-06-10 19:57:49 -0300

    Modificado Shared Project Comun

--- Servidor Torneo/ServidorTorneo.cpp
+++ Servidor Torneo/ServidorTorneo.cpp
@@ -5,5 +5,22 @@
  *      Author: ghiroma
  */
 
+#include "Clases/ServerSocket.h"
+#include "Clases/CommunicationSocket.h"
+#include "Funciones.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+  int *port = NULL;
+  char * ip = NULL;
+
+  ServerSocket sSocket();
+
+}
 
 
