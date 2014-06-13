72884d50a1a44a6ab30502acf54489f13c347ba5
ghiroma <guillermo.hiroma@gmail.com>
2014-06-13 20:23:17 -0300

    Agregado metodo para obtener codigo de mensaje

---
+++ Comun/Support/Funciones.cpp
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
+char * GetCodeFromMessage(char * message)
+{
+  char[3] code = '\0';
+  
+  if(message)
+  {
+    code[0] = message[0];
+    code[1] = message[1];
+    code[2] = '\0' ;
+  }
+  
+  return code;
+}
+
+string GetCodeFromMessage(char * message)
+{
+  string code;
+  
+  if(strlen(message)>0)
+  {
+    code.append(message[0]);
+    code.append(message[1]);
+  }
+  
+  return code;
+}
