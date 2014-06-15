28fd1b43626750ac9a3c1eeed986918edde33ea4
guillermo.hiroma <guillermo.hiroma@gmail.com>
2014-06-15 16:29:50 -0300

    Agregado constantes y reemplazadas en programa principal

--- Servidor Partida/ServidorPartida.cpp
+++ Servidor Partida/ServidorPartida.cpp
@@ -115,37 +115,36 @@ timer_thread (void* arg)
     {
       if (TimeDifference (5, startingTimeRalph) == true)
 	{
-	  //intervalo ralph.
-	  string message = "Ralph"; //Poner constantes de mensaje.
+	  string message(CD_MOVIMIENTO_RALPH);
+	  //Agregarle hacia donde se va a mover ralph
 	  timer_queue.push (message);
 	  startingTimeRalph = time (0);
 	}
 
       if (TimeDifference (20, startingTimePaloma) == true)
 	{
-	  //intervalo pajaro.
-	  string message ("Pajaro");
+	  string message(CD_PALOMA);
+	  //Agregarle donde aparece
 	  timer_queue.push (message);
-	  startingTimePaloma = time(0);
+	  startingTimePaloma = time (0);
 	}
 
       if (TimeDifference (35, startingTimeTorta) == true)
 	{
-	  //intervalo torta.
-	  string message ("Torta");
+	  string message (CD_TORTA);
+	  //Agregarle donde aparece.
 	  timer_queue.push (message);
-	  startingTimeTorta = time(0);
+	  startingTimeTorta = time (0);
 	}
 
       if (TimeDifference (60, startingTimePersiana) == true)
 	{
-	  //intervalo persiana.
-	  string message ("Persiana");
+	  string message (CD_PERSIANA);
+	  //Agregarle donde aparece
 	  timer_queue.push (message);
-	  startingTimePersiana = time(0);
+	  startingTimePersiana = time (0);
 	}
-      //usleep (1000);
-      sleep(1);
+      sleep (1);
     }
 
   pthread_exit (0);
@@ -154,18 +153,19 @@ timer_thread (void* arg)
 void*
 receiver1_thread (void * fd)
 {
-  //CommunicationSocket cSocket(*(int *)fd);
   char buffer[512];
+  bzero(buffer,sizeof(buffer));
 
   while (stop == false)
     {
       cSocket1->ReceiveNoBloq (buffer, sizeof(buffer));
-      string aux (buffer);
-      //TODO Realizar accion.
-      receive1_queue.push (aux);
-
-      //usleep (1000);
-      sleep(1);
+      if (strlen (buffer) > 0)
+	{
+	  string aux (buffer);
+	  //TODO Realizar accion.
+	  receive1_queue.push (aux);
+	}
+      sleep (1);
     }
 
   pthread_exit (0);
@@ -174,16 +174,20 @@ receiver1_thread (void * fd)
 void*
 receiver2_thread (void * fd)
 {
-  //CommunicationSocket cSocket(*(int *)fd);
   char buffer[512];
+  bzero(buffer,sizeof(buffer));
 
   while (stop == false)
     {
       cSocket2->ReceiveNoBloq (buffer, sizeof(buffer));
+     if(strlen(buffer)>0)
+	{
+	 string aux(buffer);
+	  receive2_queue.push(aux);
+	}
       //TODO realizar accion.
-      //receive2_queue.push_back();
       //usleep (1000);
-      sleep(1);
+      sleep (1);
     }
 
   pthread_exit (0);
@@ -197,46 +201,35 @@ sender_thread (void * arguments)
 
   while (stop == false)
     {
-      if (!receive1_queue.empty()) {
-       string message = receive1_queue.front();
-       //switch(message.c_str())
-       //{
+      if (!receive1_queue.empty ())
+	{
+	  string message = receive1_queue.front ();
+	  cout << "Mensaje recibido del receve1: " << message.c_str () << endl;
+	  cSocket1->SendBloq (message.c_str (), message.length ());
+	  receive1_queue.pop ();
+	}
 
-       //}
-       //cSocket1->SendBloq(message.c_str(), message.lenth());
-       receive1_queue.pop();
-       }
-
-
-      if (!receive2_queue.empty()) {
-       string message = receive2_queue.front();
-       //switch(message.c_str())
-       //{
-
-       //}
-       //cSocket2->SendBloq(message.c_str(),message.length());
-       receive2_queue.pop();
-       }
+      if (!receive2_queue.empty ())
+	{
+	  string message = receive2_queue.front ();
+	  //cSocket2->SendBloq(message.c_str(),message.length());
+	  receive2_queue.pop ();
+	}
 
       if (!timer_queue.empty ())
 	{
+	  //Lo que venga del timer, se replica a ambos jugadores.
 	  string message = timer_queue.front ();
 	  timer_queue.pop ();
-	  //switch(message.c_str())
-	  //{
-
-	  //}
-	  cout << "Mensaje a enviar: " << message.c_str() << endl;
-	  cSocket1->SendBloq(message.c_str(),message.length());
+	  cout << "Mensaje a enviar: " << message.c_str () << endl;
+	  cSocket1->SendBloq (message.c_str (), message.length ());
 	  //cSocket2->SendBloq(message.c_str(),message.length());
 	}
       sleep (1);
-      //usleep(1000);
     }
 
   pthread_exit (0);
 }
-
 
 void
 SIGINT_Handler (int inum)
--- Servidor Partida/Support/Constantes.h
+++ Servidor Partida/Support/Constantes.h
@@ -8,39 +8,25 @@
 #ifndef CONSTANTES_H_
 #define CONSTANTES_H_
 
-#define ARRIBA "Movimiento Arriba"
-#define ABAJO "Movimiento Abajo"
-#define DERECHA "Movimiento Derecha"
-#define IZQUIERDA "Movimiento Izquierda"
-#define REPARAR "Tecla de Reparar"
-#define SALIR "Tecla para Salir"
+const int INTERVALOS_RALPH = 5;
+const int INTERVALOS_PALOMA = 20;
+const int INTERVALOS_TORTA = 60;
+const int INTERVALOS_PERSIANA = 35;
 
-const int INTERVALOS_RALPH = 1;
-const int INTERVALOS_PALOMA = 4;
-const int INTERVALOS_LADRILLO1 = 2;
-const int INTERVALOS_LADRILLO2 = 1;
-
-enum Codigo_Mensaje {
-    Movimiento_Ralph = 00,
-    Movimiento_Felix = 01,
-    Vida_Perdida =02,
-    Torta_Comida = 03,
-    Torta_Generada = 04,
-    Persiana = 05,
-    Jugador_Desconectado = 06,
-    Nivel_Superado = 07,
-    Generar_Ventana_Rota = 10,
-    Ventana_Arreglada = 11,
-    Generar_Paloma=12
-};
-
-enum Intervalos{
-    Ralph_1 = 1,
-    Ralph_2 = 1,
-    Ladrillo_1 = 2,
-    Ladrillo_2 = 1,
-    Paloma = 2,
-};
+//CD viene de codigo mensaje.
+const char * CD_MOVIMIENTO_RALPH = "00";
+const char * CD_PALOMA = "01";
+const char * CD_TORTA = "02";
+const char * CD_PERSIANA = "03";
+const char * CD_MOVIMIENTO_FELIX = "04";
+//const char *
+//const char *
+//const char *
+//const char *
+//const char *
+//const char *
+//const char *
+//const char *
 
 
 #endif /* CONSTANTES_H_ */
