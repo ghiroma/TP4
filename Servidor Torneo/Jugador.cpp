b517728012547ae90ac9bbcb8313df58dcceb28e
miguel <m32_a77aque@hotmail.com>
2014-06-20 18:01:42 -0300

    encontrarOponente

--- Servidor Torneo/Jugador.cpp
+++ Servidor Torneo/Jugador.cpp
@@ -32,9 +32,9 @@ void Jugador::actualizarPartida(int idOponente, int ganado) {
 }
 
 void Jugador::agregarJugador(int idOponente) {
-	if(idOponente==1){
+	if (idOponente == 1) {
 		this->Partidas[idOponente] = 1;
-	}else{
+	} else {
 		this->Partidas[idOponente] = 2;
 	}
 	//this->Partidas[idOponente] = 0;
@@ -57,81 +57,53 @@ int Jugador::obtenerOponente(map<int, Jugador*>* listJugadores) {
 	for (map<int, Jugador*>::iterator it = (*listJugadores).begin(); it != (*listJugadores).end(); it++) {
 		if ((*(*it).second).Id != this->Id && !(*(*it).second).Jugando) {
 			if (this->Partidas[(*(*it).second).Id] == 0) {
-				cout << "oponente encontrado: " << (*(*it).second).Id << endl;
+				//cout << "oponente encontrado: " << (*(*it).second).Id << endl;
 				idOponenteEncontrado = (*(*it).second).Id;
 				break;
 			}
 		}
 	}
 
-	//si no encuentro ninguno
-	int idJugadorMenosVecesEnfrentado;
-	int minCantVecesEnfrentado = -1;
-	map<int, int>::iterator p = this->Partidas.begin();
-	//busco alguno que no este jugando para tomarlo como referencia(inicializar) de minCantVecesEnfrentado
-	while(p != this->Partidas.end()) {
-		if(!this->Jugando){
-			idJugadorMenosVecesEnfrentado = p->first;
-			minCantVecesEnfrentado = p->second;
-			break;
-		}
-		p++;
-	}
-	//si encontro alguno que no este jugando
-	if(minCantVecesEnfrentado != -1){
-		//busco en todo el listado el que menos veces jugo
-		p = this->Partidas.begin();
+	if (idOponenteEncontrado == -1) {
+		//si no encuentro ninguno
+		int minCantVecesEnfrentado = -1;
+		map<int, int>::iterator p = this->Partidas.begin();
+		//busco alguno que no este jugando para tomarlo como referencia(inicializar) de minCantVecesEnfrentado
 		while (p != this->Partidas.end()) {
-			//////
-			////// cambiara a MAP sino no puedo acceder al jugador para ver si esta jungando
-
-			if(!(*(*listJugadores)[p->first]).Jugando && p->second < minCantVecesEnfrentado){
-				cout << "oponente encontrado: " << p->first << endl;
-				idJugadorMenosVecesEnfrentado = p->first;
+			if (!(*listJugadores)[p->first]->Jugando) {
+				idOponenteEncontrado = p->first;
 				minCantVecesEnfrentado = p->second;
 				break;
 			}
-
-
 			p++;
+		}
+		//si encontro alguno que no este jugando
+		if (minCantVecesEnfrentado != -1) {
+			//busco en todo el listado el que menos veces jugo
+			p = this->Partidas.begin();
+			while (p != this->Partidas.end()) {
+				if (!(*listJugadores)[p->first]->Jugando && p->second < minCantVecesEnfrentado) {
+					//cout << "oponente encontrado: " << p->first << endl;
+					idOponenteEncontrado = p->first;
+					minCantVecesEnfrentado = p->second;
+					break;
+				}
+				p++;
+			}
 		}
 	}
 
-	/*
-	if (idOponenteEncontrado == -1) {
-		//contabilizo los enfrentamientos y veo con quien se enfrento menos veces (que no este jugando)
-		for (list<Jugador>::iterator it = (*listJugadores).begin(); it != (*listJugadores).end(); it++) {
-			if ((*it).Id != this->Id && !(*it).jugando) {
-
-				if ((*it).Partidas[]) {
-
-				}
-
-			}
-		}
-	}*/
-
-	//si no encuentro ninguno
-	/*if (idOponenteEncontrado == -1) {
-		//asigno al primer jugador que encuentre (que no este jugando)
-		for (map<int,Jugador>::iterator it = (*listJugadores).begin(); it != (*listJugadores).end(); it++) {
-			if ((*it).Id != this->Id && !(*it).Jugando) {
-				cout << "oponente encontrado: " << (*it).Id << endl;
-				idOponenteEncontrado = (*it).Id;
-				break;
-			}
-		}
-	}*/
-
 	//no hay oponentes posibles
-	if (idOponenteEncontrado == -1){
-		cout<<"no hay oponentes posibles"<<endl;
-		return 0;
-	}else{
+	if (idOponenteEncontrado == -1) {
+		cout << "no hay oponentes posibles" << endl;
+		return -1;
+	} else {
 		//seteo que los dos jugadores que van a enfrentarse estan jugando
+		this->Jugando = true;
+		(*listJugadores)[idOponenteEncontrado]->Jugando = true;
 
-		//devuelvo los id
-		return 1;
+		//devuelvo el o los id
+		return idOponenteEncontrado;
 	}
 
 }
