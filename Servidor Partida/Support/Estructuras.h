8d49bf937eb05d2152a6afa17faf4b7d0867a897
ghiroma <ghiroma@ghiroma-HP-Pavilion-dv6-Notebook-PC>
2014-06-22 17:53:51 -0300

    Commit archivo de estructuras

---
+++ Servidor Partida/Support/Estructuras.h
@@ -0,0 +1,18 @@
+/*
+ * Estructuras.h
+ *
+ *  Created on: Jun 20, 2014
+ *      Author: ghiroma
+ */
+
+#ifndef ESTRUCTURAS_H_
+#define ESTRUCTURAS_H_
+
+struct mensaje
+{
+	char codigo_mensaje[3];
+	char contenido[7];
+};
+
+
+#endif /* ESTRUCTURAS_H_ */
