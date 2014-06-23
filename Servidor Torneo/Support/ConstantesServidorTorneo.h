f877a81ab749013b209d463cb0261b22d2afd374
migueldp <m32_a77aque@hotmail.com>
2014-06-23 16:28:48 -0300

    Update ConstantesServidorTorneo.h

--- Servidor Torneo/Support/ConstantesServidorTorneo.h
+++ Servidor Torneo/Support/ConstantesServidorTorneo.h
@@ -33,9 +33,9 @@ static const int PERMISOS_SHM = 0777;
 static const int CLAVE_MEMORIA_COMPARTIDA = 1322;
 static const int INTERVALO_ENTRE_BUSQUEDA_DE_OPONENTES = 1000000;
 
-static const int ANCHO_PANTALLA	= 800;
-static const int ALTO_PANTALLA = 600;
-static const int BPP = 8;
+static const int ANCHO_PANTALLA_SERVIDOR = 800;
+static const int ALTO_PANTALLA_SERVIDOR = 600;
+static const int BPP_SERVIDOR = 8;
 
 
 #endif /* CONSTANTESSERVIDORTORNEO_H_ */
